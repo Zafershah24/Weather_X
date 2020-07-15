49e0291a5c662df7508a6663f06c8ffee3d6c1a1
zafer247 <zafershah247@gmail.com>
2020-07-16 04:27:08 +0530

    Final update

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:weatherx/screens/LoadScreen.dart';
-import 'package:weatherx/screens/locationscreen.dart';
-import 'package:geolocator/geolocator.dart';
+
 
 
 void main() => runApp(MyApp());
